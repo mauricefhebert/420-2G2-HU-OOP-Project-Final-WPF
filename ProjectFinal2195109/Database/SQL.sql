75b104af2c46fd8ffa48c544391428d9a0d896b4
Mauricefh <filiatreaultmaurice@gmail.com>
2022-04-04 19:54:37 -0400

    backup before doing a scallars

--- ProjectFinal2195109/Database/SQL.sql
+++ ProjectFinal2195109/Database/SQL.sql
@@ -10,11 +10,13 @@ create table Users (
 
 create table Shopping_List(
 	ShoppingListID int primary key identity,
+	IngrediantID int FOREIGN KEY REFERENCES Ingrediant(IngrediantID),
 	UserID int FOREIGN KEY REFERENCES Users(UserID)
 );
 
 create table List_Item(
 	ListItemID int primary key identity,
+	QuantityID int FOREIGN KEY REFERENCES Quantity(QuantityID),
 	ShoppingListID int FOREIGN KEY REFERENCES Shopping_List(ShoppingListID)
 );
 
@@ -35,7 +37,7 @@ create table Ingrediant(
 
 create table Measurement(
 	MeasurementID int primary key identity,
-	MeasurementUnit int not null unique,
+	MeasurementUnit varchar(50) not null unique,
 );
 
 create table Quantity(
@@ -56,3 +58,50 @@ create index idx_RecipeID on Quantity (RecipeID);
 create index idx_IngrediantID on Quantity (IngrediantID);
 create index idx_MeasurementID on Quantity (MeasurementID);
 
+--Creation des unités de mesure
+insert into Measurement (MeasurementUnit) Values ('Tablespoon');
+insert into Measurement (MeasurementUnit) Values ('Teaspoon');
+insert into Measurement (MeasurementUnit) Values ('Ounce');
+insert into Measurement (MeasurementUnit) Values ('Fluid ounce');
+insert into Measurement (MeasurementUnit) Values ('Cup');
+insert into Measurement (MeasurementUnit) Values ('Quart');
+insert into Measurement (MeasurementUnit) Values ('Pint');
+insert into Measurement (MeasurementUnit) Values ('Gallon');
+insert into Measurement (MeasurementUnit) Values ('Pound');
+insert into Measurement (MeasurementUnit) Values ('Milliliter');
+insert into Measurement (MeasurementUnit) Values ('Grams');
+insert into Measurement (MeasurementUnit) Values ('Kilogram');
+insert into Measurement (MeasurementUnit) Values ('Liter');
+insert into Measurement (MeasurementUnit) Values ('Whole');
+
+--Get every recipe assosiated with a user
+create procedure getRecipeByUser @UserID int
+as 
+begin
+select * from Users
+join Recipe
+on Users.UserID = Recipe.UserID
+where Users.UserID = @UserID
+end
+go
+
+--Get all the item in the list of a user
+create procedure getItemInListForUser @UserID int
+as 
+begin
+select QuantityValue,MeasurementUnit,IngrediantName,Title from Users
+join Shopping_List
+on Shopping_List.UserID = Users.UserID
+join List_Item
+on Shopping_List.UserID = List_Item.ShoppingListID
+join Quantity
+on List_Item.QuantityID = Quantity.QuantityID
+join Measurement
+on Measurement.MeasurementID = Quantity.MeasurementID
+join Ingrediant
+on Ingrediant.IngrediantID = Quantity.QuantityID
+join Recipe
+on Recipe.RecipeID = Quantity.RecipeID
+where Users.UserID = @UserID
+end
+go
