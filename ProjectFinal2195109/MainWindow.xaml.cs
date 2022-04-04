684350bbf23d68fe8a9222431ec867780c96f85d
Mauricefh <filiatreaultmaurice@gmail.com>
2022-04-04 17:22:27 -0400

    Validation Creation de compte terminer

--- ProjectFinal2195109/MainWindow.xaml.cs
+++ ProjectFinal2195109/MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace ProjectFinal2195109
             InitializeComponent();
         }
 
+        //Theme
+
         //Allow the change of the theme with a toggle button
         public bool IsDarkTheme { get; set; }
         private readonly PaletteHelper paletteHelper = new PaletteHelper();
@@ -46,16 +48,34 @@ namespace ProjectFinal2195109
             paletteHelper.SetTheme(theme);
         }
 
-        //Allow to exit the application
-        private void exitApp(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
-
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
             DragMove();
+        }
+
+        //Global
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //loginPage.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Grid x in Container.Children)
+            {
+                if (x is Grid)
+                {
+                    x.Visibility = Visibility.Hidden;
+                }
+            }
+            loginPage.Visibility = Visibility.Visible;
+        }
+
+        //Allow to exit the application
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
 
         //Login page
@@ -79,14 +99,19 @@ namespace ProjectFinal2195109
             {
                 loginPage.Visibility = Visibility.Hidden;
                 recipeListPage.Visibility = Visibility.Visible;
+                errorUsernameLogin.Visibility = Visibility.Hidden;
+                errorPasswordLogin.Visibility = Visibility.Hidden;
+                txtUsernameLoginPage.Text = string.Empty;
+                txtPasswordLoginPage.Password = string.Empty;
             }
             else if (!dbContext.Users.Any(u => u.Username == user.Username))
             {
-                MessageBox.Show("Utilisateur inexistant");
+                errorUsernameLogin.Visibility = Visibility.Visible;
             }
             else if (dbContext.Users.Any(u => u.Username == user.Username && u.Password != user.Password))
             {
-                MessageBox.Show("Mot de passe invalide");
+                errorUsernameLogin.Visibility = Visibility.Hidden;
+                errorPasswordLogin.Visibility = Visibility.Visible;
             }
 
         }
@@ -98,6 +123,89 @@ namespace ProjectFinal2195109
         {
             createAccountPage.Visibility = Visibility.Hidden;
             loginPage.Visibility = Visibility.Visible;
+        }
+        private void btnConfirmSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User()
+            {
+                Email = txtCourrielCreateAccountPage.Text,
+                Username = txtUsernameCreateAccountPage.Text,
+                Password = txtPasswordCreateAccountPage.Password,
+            };
+            if (validationCourriel() && validationUsername() && validationPassword())
+            {
+                dbContext.Users.Add(user);
+                dbContext.SaveChanges();
+                txtCourrielCreateAccountPage.Text = string.Empty;
+                txtUsernameCreateAccountPage.Text = string.Empty;
+                txtPasswordCreateAccountPage.Password = string.Empty;
+                createAccountPage.Visibility = Visibility.Hidden;
+                recipeListPage.Visibility = Visibility.Visible;
+            }
+        }
+        public bool validationCourriel()
+        {
+            bool valid = false;
+            User user = new User() { Email = txtCourrielCreateAccountPage.Text };
+            if (txtCourrielCreateAccountPage.Text == string.Empty)
+            {
+                errorEmailCreateAccount.Visibility = Visibility.Visible;
+                errorEmailCreateAccount.Text = "Le courriel ne peu etre vide";
+            }
+            else if (dbContext.Users.Any(u => u.Email == user.Email))
+            {
+                errorEmailCreateAccount.Visibility = Visibility.Visible;
+                errorEmailCreateAccount.Text = "Le courriel entrée est associées a un compte";
+            }
+            else
+            {
+                errorEmailCreateAccount.Visibility = Visibility.Hidden;
+                valid = true;
+            }
+            return valid;
+        }
+
+        public bool validationUsername()
+        {
+            bool valid = false;
+            var user = new User() { Username = txtUsernameCreateAccountPage.Text };
+            if (txtUsernameCreateAccountPage.Text == string.Empty)
+            {
+                errorUsernameCreateAccount.Visibility = Visibility.Visible;
+                errorUsernameCreateAccount.Text = "Le nom d'utilisateur ne peu etre vide";
+            }
+            else if (dbContext.Users.Any(u => u.Username == user.Username))
+            {
+                errorUsernameCreateAccount.Visibility = Visibility.Visible;
+                errorUsernameCreateAccount.Text = "L'utilisateur entrée est associées a un compte";
+            }
+            else
+            {
+                errorUsernameCreateAccount.Visibility = Visibility.Hidden;
+                valid = true;
+            }
+            return valid;
+        }
+
+        public bool validationPassword()
+        {
+            bool valid = false;
+            if (txtPasswordCreateAccountPage.Password == string.Empty)
+            {
+                errorPasswordCreateAccount.Visibility = Visibility.Visible;
+                errorPasswordCreateAccount.Text = "Le mot de passe ne peu etre vide";
+            }
+            else if (txtPasswordCreateAccountPage.Password.Length < 8 || txtPasswordCreateAccountPage.Password.Length > 20)
+            {
+                errorPasswordCreateAccount.Visibility = Visibility.Visible;
+                errorPasswordCreateAccount.Text = "Le mot de passe dois etre entre 8 et 20 caracters";
+            }
+            else
+            {
+                errorPasswordCreateAccount.Visibility = Visibility.Hidden;
+                valid = true;
+            }
+            return valid;
         }
     }
 }
